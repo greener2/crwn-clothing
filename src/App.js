97c7153a8cf6d4fd8cfe585d0e327d72f7e52799
Rob Greene <robert.greene@giant.ie>
2020-02-14 14:11:50 +0000

    Added sign in form with proper styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 const HatsPage = () => (
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signIn' component={SignInSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+
+const SignInSignUpPage = () => (
+    <div className="sisu">
+        <SignIn />
+    </div>
+);
+
+export default SignInSignUpPage;
